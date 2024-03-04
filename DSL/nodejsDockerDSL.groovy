12582204e12283bbe7c69ca0a518cbd3ce575c1e
Junior <nahimjardines@hotmail.com>
2024-03-04 18:49:08 +0100

    Update nodejsDockerDSL.groovy

--- DSL/nodejsDockerDSL.groovy
+++ DSL/nodejsDockerDSL.groovy
@@ -16,7 +16,7 @@ job('Aplicacion Node.js Docker DSL') {
         dockerBuildAndPublish {
             repositoryName('nahimjardines/nodejsapp')
             tag('${GIT_REVISION,length=7}')
-            registryCredentials('docker-hub')
+            registryCredentials('softengineer')
             forcePull(false)
             createFingerprints(false)
             skipDecorate()
